2fa5377ab3727f1a51088c3ea19bf89742730ffc
Jessé Silva <jessepescador22@gmail.com>
2023-03-17 10:36:23 -0300

    feito as raquete direita e esquerda do projeto

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function setup() {
 }
 
 function draw() {
+  // desenha o campo
   canvasCtx.fillStyle = "#03351e";
   canvasCtx.fillRect(0, 0, window.innerWidth,window.innerHeight );
 
   canvasCtx.fillStyle = "#ffffff";
+
+  // desenha linha central
 
   const x = window.innerWidth / 2 - lineWidth / 2 ;
   const y = 0;
@@ -19,6 +22,13 @@ function draw() {
   const h = window.innerHeight;
 
   canvasCtx.fillRect(x, y, w, h);
+
+  // desenha a raquete esquerda
+  canvasCtx.fillRect(10, 350, lineWidth, 200);
+  // desenha a raquete direita 
+  canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 200, lineWidth, 200);
+
+
 }
 
 setup();
