84a39d725a590d0c30ba4005b849db82cafa13ca
Jessé Silva <jessepescador22@gmail.com>
2023-03-17 11:06:25 -0300

    foi realizado o placar para teste

--- script.js
+++ script.js
@@ -29,10 +29,22 @@ function draw() {
   canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 200, lineWidth, 200);
 
   // fazendo a bolinha 
-
   canvasCtx.beginPath();
   canvasCtx.arc(200, 300, 20, 0, 2 * Math.PI, false);
   canvasCtx.fill()
+
+  // desenhando o placar 
+
+  canvasCtx.font = "bold 72px Arial";
+  canvasCtx.textAlign = "center";
+  canvasCtx.textBaseline = "top";
+  canvasCtx.fillStyle = " #000000";
+  canvasCtx.fillText("3", window.innerWidth / 4, 50);
+  canvasCtx.fillText(
+    "1",
+    window.innerWidth / 4 + window.innerWidth / 2, 50
+  )
+ 
 
 
 }
